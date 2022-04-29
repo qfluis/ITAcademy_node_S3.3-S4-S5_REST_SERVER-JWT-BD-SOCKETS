308fd640401a50a603fef5ee1ec79155ab0c9771
Luis Quevedo <qfluis@gmail.com>
2022-04-29 18:52:49 +0200

    Pinta y colorea

--- app/routes/web.routes.js
+++ app/routes/web.routes.js
@@ -4,6 +4,7 @@ const { get404 } = require ('../controllers/web.controllers');
 const router = Router();
 
 // 404
+// TODO: ¿Crear ruta /404 para que no se vea 404.html?
 router.get('*', get404);
 
 module.exports = router;
