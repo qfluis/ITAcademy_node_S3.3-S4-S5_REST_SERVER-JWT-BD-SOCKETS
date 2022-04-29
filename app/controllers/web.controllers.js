228cd3f030e5d6251daaa78a9127698662802a96
Luis Quevedo <qfluis@gmail.com>
2022-04-29 17:02:02 +0200

    parcheado 404

--- app/controllers/web.controllers.js
+++ app/controllers/web.controllers.js
@@ -2,11 +2,13 @@ const { response, request } = require('express');
 const path = require('path');
 
 
-// TODO: imágenes se rompen si el path tiene más de una "carpeta"
+// TODO: Revisar si es la mejor manera aparece el 404.html en la ruta...
 const get404 = (req = request, res = response) => {
+    /*
     const url = path.resolve('./app/public/404.html');
     console.log("WEB", url);
-    res.sendFile(url);
+    res.sendFile(url);*/
+    res.redirect("/404.html");
 }
 
 module.exports = { get404 }
