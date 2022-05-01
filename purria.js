ebb7bae6e8da03c80823cd90e7593eee33021fe9
Patricia <waltraute@hotmail.com>
2022-05-01 21:49:29 +0200

    modificat

--- purria.js
+++ purria.js
@@ -101,19 +101,7 @@ Tenemos un array numbers, en el cuál insertaremos números. Insertaremos la can
  lanzamiento de un dado (un número aleatorio del 1 al 6). En el caso de obtener un 6, paramos y rompemos 
  el bucle 
 
- const iterations = 10;
- let numbers = [];
  
- for (let i = 0; i < iterations; i++) {
-   const number = 1 + Math.floor(Math.random() * 6);
-   numbers.push(number);
-   if (number === 6) {
-     console.log("ERROR");
-     break;
-   }
- }
- 
- console.log(numbers);
 
 
 */
