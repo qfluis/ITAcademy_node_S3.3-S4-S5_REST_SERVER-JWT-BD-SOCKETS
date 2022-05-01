1734c335874e03943a579e76ab647bad2d1357e9
Patricia <waltraute@hotmail.com>
2022-05-01 18:27:48 +0200

    crear model player

---
+++ models/player.js
@@ -0,0 +1,16 @@
+"use strict";
+
+class Player {
+
+    constructor(){
+        this.id = id
+        this.nom = nom
+        this.data_registre = data_registre
+
+    }
+
+
+
+}
+
+module.exports = Player
