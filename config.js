3926b8aa262bd45f49bca36c1c6f11d497523ddb
Patricia <waltraute@hotmail.com>
2022-05-08 21:10:46 +0200

    afegir variables db

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
-module.exports = {port: process.env.PORT || 8000}
-
-
+module.exports = {port: process.env.PORT || 8000,
+    user:"pat",
+    password:"Pat12345678_",
+    database:"dices2",
+    host:"localhost",
+    dialect: 'mysql'
+}
