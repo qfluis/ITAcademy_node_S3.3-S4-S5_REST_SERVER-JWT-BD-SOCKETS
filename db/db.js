c72803a1af029ec4e7e39d7197e31b2699da39fd
Patricia <waltraute@hotmail.com>
2022-05-09 09:49:51 +0200

    afegir require db

--- db/db.js
+++ db/db.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 const config = require('./config')
 
 const PlayerMdl = require('./db/models/playerMdl')
+const MatchesMdl = require('./db/models/matchesMdl')
 
 const sequelize = new Sequelize(config.user, config.database, config.password, {
 
@@ -13,5 +14,20 @@ const sequelize = new Sequelize(config.user, config.database, config.password, {
 
 const Player1 = PlayerMdl(sequelize, Sequelize)
 
+sequelize.sync({ force: false })
+.then(() => {
+console.log('Synchronized tables');
+})
+
+const Match1 = MatchesMdl(sequelize, Sequelize)
+
+sequelize.sync({ force: false })
+.then(() => {
+console.log('Synchronized tables');
+})
+
+module.exports = {
+  Player1, Match1
+}
 
 
