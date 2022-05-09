51dea44181713867d270d3a2fcc0a5df60fa6c66
Patricia <waltraute@hotmail.com>
2022-05-09 09:49:09 +0200

    modificar

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express()
+require('./db')
 const config = require('./config')
 const bodyParser = require('body-parser');
 //require ( 'dotenv' ).config()
 const playersPost = require('./controllers/playerCtrl')
 const playersGet = require('./controllers/playerCtrl')
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
