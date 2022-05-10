44ae3a62f719bb4c33252f4993e90615ad25e922
Patricia <waltraute@hotmail.com>
2022-05-10 12:18:29 +0200

    config modificat

--- config.js
+++ config.js
@@ -1,1 +1,10 @@
-module.exports = {port: process.env.PORT || 3306}
+const config = {
+  user: "root",
+  password: "root1111_",
+  database: "dice_game",
+  host: "localhost",
+  dialect: "mysql",
+  port: process.env.PORT || 3306,
+}
+
+module.exports = config
