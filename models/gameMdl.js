2adb6f8dd47cf78e1f689fa3ad061b67acecb659
Patricia <waltraute@hotmail.com>
2022-05-12 00:07:42 +0200

    creat gameMdl

---
+++ models/gameMdl.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, type) => {
+    return sequelize.define('games', {
+        idGames: {
+            type: type.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        dice1: type.INTEGER,
+        dice2: type.INTEGER,
+       // idPlayer: type.INTEGER,  
+       })}
+
+
