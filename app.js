932456b550452b88190613b0a5aa2ed51c2dc5c9
Patricia <waltraute@hotmail.com>
2022-05-12 00:38:22 +0200

    modificats

--- app.js
+++ app.js
@@ -1,34 +1,28 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 const config = require("./config");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 require("./database/db");
-const router = express.Router();
 
 const apiRouter = require("./routes/api");
 const playersPost = require("./controllers/playerCtrl");
 const playersGet = require("./controllers/playerCtrl");
-
-
-
-
-
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api", apiRouter);
 
-app.post("/players", playersPost);
+/*app.post("/players", playersPost);
 app.get("/players", playersGet);
 
 app.get("/*", error);
 app.post("/*", error);
 app.put("/*", error);
 app.delete("/*", error);
-
+*/
 app.listen(config.port, () => {
   console.log(`API REST en http://localhost:${config.port}/`);
 });
