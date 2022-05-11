3fce8f8d95b33ebb397cb853b086241e92bbbe92
Patricia <waltraute@hotmail.com>
2022-05-11 19:57:59 +0200

    modificat

--- models/playerMdl.js
+++ models/playerMdl.js
@@ -1,27 +1,11 @@
-"use strict";
-
-class Player {
-
-    constructor(name){
-        this.id = null
-        this.name = name
-        this.register_date = new Date
-    }
-
-
-
-       module.exports = (sequelize, type) => {
-        return sequelize.define('player', {
-            idPlayer: {
-                type: type.INTEGER,
-                primaryKey: true,
-                autoIncrement: true
-            },
-        namePlayer: type.VARCHAR,
-        registerDate: type.DATE
-    })}
-
-}     
-
-
-module.exports = Player
+module.exports = (sequelize, type) => {
+  return sequelize.define("player", {
+    idPlayer: {
+      type: type.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    namePlayer: type.STRING,
+    registerDate: type.DATE,
+  });
+};
