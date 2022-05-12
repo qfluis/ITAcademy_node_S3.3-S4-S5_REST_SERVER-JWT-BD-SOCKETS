816cc470f4622e0de3a617ef371330163cf8c3b5
Patricia <waltraute@hotmail.com>
2022-05-12 03:28:30 +0200

    afegir class

---
+++ models/classPlayer.js
@@ -0,0 +1,11 @@
+"use strict";
+
+class Player {
+
+    constructor(name){
+        this.id = null
+        this.name = name
+        this.register_date = new Date
+    }
+       }
+module.exports = Player
