929a7a96f5aa791c817d5231dacd949a4b45fda2
Patricia <waltraute@hotmail.com>
2022-05-12 03:19:28 +0200

    modificar arxius db

---
+++ db.js
@@ -0,0 +1,41 @@
+const Sequelize = require("sequelize");
+
+const config_db = require("./config");
+
+const PlayerMdl = require("./models/playerMdl");
+const GameMdl = require("./models/gameMdl");
+
+const sequelize = new Sequelize(
+  config_db.database,
+  config_db.user,
+  config_db.password,
+  {
+    host: config_db.host,
+    dialect: config_db.dialect,
+  }
+);
+
+const Player1 = PlayerMdl(sequelize, Sequelize);
+const Game1 = GameMdl(sequelize, Sequelize);
+
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("Synchronized tables");
+  })
+  .catch((error) => {
+    console.log("Has occurred an error", error);
+  });
+
+sequelize
+  .drop()
+  .then(() => {
+    console.log("Deleted tables");
+  })
+  .catch((error) => {
+    console.log("Has occurred an error", error);
+  });
+
+module.exports = {
+  Player1, Game1
+};
